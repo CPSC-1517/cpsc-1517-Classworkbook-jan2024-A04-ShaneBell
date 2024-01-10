835ea4c6680166f41db823256aed7801c1753cbe
Shane Bell <sbell@nait.ca>
2024-01-10 15:59:15 -0700

    enums

---
+++ HockeySolution/HockeyData/HockeyPlayer.cs
@@ -0,0 +1,23 @@
+﻿ namespace Hockey.Data
+{
+    public class HockeyPlayer
+    {
+        //Data fields
+        //store the data in the object
+        //prefix _ for private fields
+        //camel case
+        //The green squiggles are saying they are strings and therefore not nullable
+        //can use ? after string to allow nulls
+        //This can also be managed in the constructor
+        private string? _birthPlace;
+        private string? _firstName;
+        private string? _lastName;
+        private int _heightInInches;
+        private int _weightInPounds;
+        private DateOnly _dateOfBirth;
+        private Position _position;
+
+
+
+    }
+}
---
+++ HockeySolution/HockeyData/Position.cs
@@ -0,0 +1,13 @@
+﻿namespace Hockey.Data
+{
+	//the underlying value for an enumeration is an integer
+	public enum Position
+	{ 
+		//By default the values start at 0
+		LeftWing=1,//can explicitly set the values
+		RightWing,
+		Center,
+		Defense,
+		Goalie
+	}
+}
