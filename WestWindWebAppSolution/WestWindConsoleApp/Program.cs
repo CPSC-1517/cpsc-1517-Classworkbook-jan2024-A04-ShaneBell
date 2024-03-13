e3a4729afc5dec88dae86f8f96dcbc40ed38ed28
Shane Bell <sbell@nait.ca>
2024-03-13 16:32:11 -0600

    WestWind

---
+++ WestWindWebAppSolution/WestWindConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+WestWindContext context = new WestWindContext();
+
+foreach(Customer aCustomer in context.Customers)
+{
+    Console.WriteLine(aCustomer.ContactName);
+}
+
+Customer firstCustomer = context.Customers.First();
+Console.WriteLine($"The first customer is: {firstCustomer.ContactName}");
