58765f5e2b12fa063ca5ed041e157d42099e3d1f
ShaneBell <sbell@nait.ca>
2024-03-18 16:00:56 -0600

    Add files via upload

--- WestWindWebAppSolution/WestWindConsoleApp/Program.cs
+++ WestWindWebAppSolution/WestWindConsoleApp/Program.cs
@@ -1,12 +1,33 @@
-﻿using WestWindSystem.DAL;
+﻿
+#region Additional Namespaces
+using WestWindSystem.DAL;
 using WestWindSystem.Entities;
+using WestWindSystem.BLL;
+#endregion
 
+//Creating a context object using the default constructor.
+//It will use the connection string in the OnConfiguring method
 WestWindContext context = new WestWindContext();
 
-foreach(Customer aCustomer in context.Customers)
+//Instantiate a customerServices Object and pass in the context of where the data is. 
+CustomerServices customerServices = new CustomerServices(context);
+
+//Display all the customers contactNames
+
+foreach(Customer aCustomer in customerServices.GetAllCustomers())
 {
     Console.WriteLine(aCustomer.ContactName);
 }
 
-Customer firstCustomer = context.Customers.First();
-Console.WriteLine($"The first customer is: {firstCustomer.ContactName}");
+Customer customer2 = customerServices.GetCustomerById("ALfki");
+
+Console.WriteLine($"ALFKI is {customer2.ContactName}");
+
+//foreach(Customer aCustomer in context.Customers)
+//{
+//    Console.WriteLine(aCustomer.ContactName);
+//}
+
+
+//Customer firstCustomer = context.Customers.First();
+//Console.WriteLine($"The first customer is: {firstCustomer.ContactName}");
---
+++ WestWindWebAppSolution/WestWindSystem/BLL/CustomerServices.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+#endregion
+
+
+namespace WestWindSystem.BLL
+{
+    public class CustomerServices
+    {
+        //Create a field for the property to hold our context
+        //readonly since we should not be able to update the context outside the WestWindContext Class
+
+        private readonly WestWindContext _context;
+
+        public CustomerServices(WestWindContext context)
+        {
+            _context = context;
+        }
+
+        //Method to return all the customers
+        public List<Customer> GetAllCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        public Customer GetCustomerById(string id)
+        {
+            id = id.ToLower();
+
+            Customer? customer = _context.Customers.Where(C => C.CustomerId.ToLower() == id).FirstOrDefault();
+//FirstOrDefault because if not record is found you can't return the first() of nothing. If that happens return the default for an object which is null
+            return customer;
+        }
+
+
+
+    }
+}
